edf3564f8590b28375d724f918a2d9eec49be549
jeffer <jeffer.8a@gmail.com>
2015-03-11 16:25:37 -0430

    database conection

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'arunbakery',
+			'database'  => 'arun',
 			'username'  => 'root',
 			'password'  => 'pass',
 			'charset'   => 'utf8',
