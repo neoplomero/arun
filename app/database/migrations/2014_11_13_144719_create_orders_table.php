016e26c45d4bc71f3cce412c2e59868a6bccc01c
vagrant <vagrant@homestead>
2015-10-06 14:55:29 +0000

    migration

--- app/database/migrations/2014_11_13_144719_create_orders_table.php
+++ app/database/migrations/2014_11_13_144719_create_orders_table.php
@@ -5,78 +5,78 @@ use Illuminate\Database\Schema\Blueprint;
 
 class CreateOrdersTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('orders', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->string('delivery_address');
-			$table->date('delivery_date');
-			$table->date('billing date');
-			$table->float('amount');
-			$table->integer('customer_id')->unsigned();
-			$table->integer('order_id')->unsigned();
-			$table->enum('payment',['pending payment','paid']);
-			$table->string('number');
-			$table->enum('email',['sent','pending'])->default('pending');
-			$table->enum('type',['order','model'])->default('order');
-			$table->string('model');
-			$table->text('note');
-			//$table->foreign('customer_id')->references('id')->on('customers');
-			//$table->foreign('order_id')->references('id')->on('orders');
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('delivery_address');
+            $table->date('delivery_date');
+            $table->date('billing_date');
+            $table->float('amount');
+            $table->integer('customer_id')->unsigned();
+            $table->integer('order_id')->unsigned();
+            $table->enum('payment', ['pending payment','paid']);
+            $table->string('number');
+            $table->enum('email', ['sent','pending'])->default('pending');
+            $table->enum('type', ['order','model'])->default('order');
+            $table->string('model');
+            $table->text('note');
+            //$table->foreign('customer_id')->references('id')->on('customers');
+            //$table->foreign('order_id')->references('id')->on('orders');
 
-			$table->timestamps();
+            $table->timestamps();
 
-		});
-		
-		Schema::create('details', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->integer('quantity')->unsigned();
-			$table->float('single_price');
-			$table->float('total_price');
-			$table->integer('order_id')->unsigned();
-			$table->integer('product_id')->unsigned();
-			$table->enum('type',['sale','devolution','credit']);
-			//$table->foreign('order_id')->references('id')->on('orders');
-			//$table->foreign('product_id')->references('id')->on('products');
+        });
+        
+        Schema::create('details', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('quantity')->unsigned();
+            $table->float('single_price');
+            $table->float('total_price');
+            $table->integer('order_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+            $table->enum('type',['sale','devolution','credit']);
+            //$table->foreign('order_id')->references('id')->on('orders');
+            //$table->foreign('product_id')->references('id')->on('products');
 
-			$table->timestamps();
-			
-		});
+            $table->timestamps();
+            
+        });
 
-		Schema::create('status', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->enum('status', ['received','processing','out for delivery','delivered','cancelled','open','standing']);
-			$table->integer('order_id')->unsigned();
-			$table->integer('user_id')->unsigned();
-			$table->string('note');
+        Schema::create('status', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->enum('status', ['received','processing','out for delivery','delivered','cancelled','open','standing']);
+            $table->integer('order_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->string('note');
 
-			//$table->foreign('order_id')->references('id')->on('orders');
-			//$table->foreign('user_id')->references('id')->on('users');
+            //$table->foreign('order_id')->references('id')->on('orders');
+            //$table->foreign('user_id')->references('id')->on('users');
 
-			$table->timestamps();
-			
-		});
-	}
+            $table->timestamps();
+            
+        });
+    }
 
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('status');
-		Schema::drop('details');
-		Schema::drop('orders');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('status');
+        Schema::drop('details');
+        Schema::drop('orders');
+    }
 
 }
